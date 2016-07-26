410efdab89baeaa31860947eb50abc761ff571fa
jesusalc <jesusalc@gmail.com>
2016-07-26 09:15:49 +0200

    fixed uninitialized constant User::FILL_IN issue

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,5 @@
 class User < ApplicationRecord
   has_many :microposts
-  validates FILL_IN, presence: true    # Replace FILL_IN with the 
-right code.
-  validates FILL_IN, presence: true    # Replace FILL_IN with the 
-right code.
+  validates "Help | Ruby on Rails Tutorial Sample App", presence: true    # Replace FILL_IN with the right code.
+  validates "Ruby on Rails Tutorial Sample App", presence: true    # Replace FILL_IN with the right code.
 end
